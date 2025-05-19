6a9be9e74977fd37b4a0d488f9b4e9678e11ad13
Zyooon <wcw7373@gmail.com>
2025-05-19 11:32:44 +0900

    feat: Entity 설정
    User 및 Board Entity 추가

---
+++ src/main/java/com/calendardev/calendardevelop/entity/Board.java
@@ -0,0 +1,22 @@
+package com.calendardev.calendardevelop.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "board")
+public class Board extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
---
+++ src/main/java/com/calendardev/calendardevelop/entity/User.java
@@ -0,0 +1,20 @@
+package com.calendardev.calendardevelop.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
