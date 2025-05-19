b3e9a5562e1fd237b9c2478661ddfeb78c920789
Zyooon <wcw7373@gmail.com>
2025-05-19 10:54:32 +0900

    init commit

---
+++ src/test/java/com/calendardev/calendardevelop/CalendarDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendardev.calendardevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
