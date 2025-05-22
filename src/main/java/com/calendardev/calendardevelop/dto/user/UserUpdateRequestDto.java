baea528351f5d6bfadb6c4555d6b659b2d088e61
Zyooon <wcw7373@gmail.com>
2025-05-22 10:32:51 +0900

    fix : 유저 업데이트 변경
    - 유저 업데이트에도 유효성 검사하도록 수정

--- src/main/java/com/calendardev/calendardevelop/dto/user/UserUpdateRequestDto.java
+++ src/main/java/com/calendardev/calendardevelop/dto/user/UserUpdateRequestDto.java
@@ -1,16 +1,19 @@
 package com.calendardev.calendardevelop.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class UserUpdateRequestDto {
 
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,10}$", message = "이름은 특수문자를 제외한 2~10자리여야 합니다.")
     private final String username;
 
     @NotBlank(message = "현재 비밀번호를 입력해주세요.")
     private final String oldPassword;
 
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문, 숫자, 특수문자를 사용하세요.")
     private final String newPassword;
 
     public UserUpdateRequestDto(String username, String oldPassword, String newPassword) {
