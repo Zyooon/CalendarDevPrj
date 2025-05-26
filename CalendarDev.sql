9330eb217c8b636a423eb6e0240d39ff4ce57090
Zyooon <wcw7373@gmail.com>
2025-05-26 14:23:18 +0900

    fix : sql 문서 수정
    - 컬럼 위치 변경

--- CalendarDev.sql
+++ CalendarDev.sql
@@ -3,35 +3,35 @@ DROP TABLE IF EXISTS `board`;
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
-  `created_at` datetime(6) DEFAULT NULL,
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `modified_at` datetime(6) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime(6) DEFAULT NULL,
+  `modified_at` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UKob8kqyqqgmefl0aco34akdtpe` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `board` (
-  `created_at` datetime(6) DEFAULT NULL,
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `modified_at` datetime(6) DEFAULT NULL,
   `user_id` bigint DEFAULT NULL,
-  `contents` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
+  `contents` varchar(255) NOT NULL,
+  `created_at` datetime(6) DEFAULT NULL,
+  `modified_at` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_board_user` (`user_id`),
   CONSTRAINT `FK_board_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `comment` (
-  `board_id` bigint DEFAULT NULL,
-  `created_at` datetime(6) DEFAULT NULL,
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `modified_at` datetime(6) DEFAULT NULL,
   `user_id` bigint DEFAULT NULL,
+  `board_id` bigint DEFAULT NULL,
   `contents` varchar(255) DEFAULT NULL,
+  `created_at` datetime(6) DEFAULT NULL,
+  `modified_at` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_comment_board` (`board_id`),
   KEY `FK_comment_user` (`user_id`),
